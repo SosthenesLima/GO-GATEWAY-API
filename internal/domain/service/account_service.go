7157afd83c814196e09bbc8b5501d31d165fa886
SosthenesLima <sosthenes.lima@gmail.com>
2025-04-16 14:59:04 -0300

    Commit_Account_service

--- internal/domain/service/account_service.go
+++ internal/domain/service/account_service.go
@@ -1,6 +1,9 @@
 package service
 
-import "github.com/devfullcycle/imersao22/go-gateway/internal/domain"
+import (
+	"github.com/devfullcycle/imersao22/go-gateway/internal/domain"
+	"github.com/devfullcycle/imersao22/go-gateway/internal/dto"
+)
 
 type AccountService struct {
 	repository domain.AcoountRepository
@@ -8,4 +11,20 @@ type AccountService struct {
 
 func NewAccountService(repository domain.AcoountRepository) *AccountService {
 	return &AccountService{repository: repository}
+}
+
+func (s *AccountService) CreateAccount(input dto.CreateAccountInput) (*dto.AccountOutput, error) {
+	account := dto.ToAccount(input)
+
+	existingAccount, err := s.repository.FindByAPIKey(account.APIKey)
+	if err != nil && err != domain.ErrAccountNotFound {
+		return nil, err
+	}
+	if existingAccount != nil {
+		return nil, domain.ErrDuplitedAPIKey
+
+	}
+
+	err = s.repository.Save()
+
 }
